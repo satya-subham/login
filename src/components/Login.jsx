f5e2777cc13039fa4c838ea0d8e63faa9185a599
satya-subham <satyasubham85@gmail.com>
2022-12-07 15:47:16 +0530

    login logout done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,49 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [login, setLogin] = useState(false);
-
-    const handleSubmit = (e) =>{
-       e.preventDefault();
-       if(username.length > 4 && password.length > 4){
-        setLogin(true)
-       }
-    };
-
-    const handleLogout = () => {
-        setLogin(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username.length > 4 && password.length > 4) {
+      setLogin(true);
     }
+  };
 
-    const handleUsername = (e) =>{
-        setUsername(e.target.value);
-    };
+  const handleLogout = () => {
+    setUsername('');
+    setPassword('');
+    if (setLogin) {
+      setLogin(false);
+    }
+  };
 
-    const handlePassword = (e) =>{
-        setPassword(e.target.value);
-    };
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
   return (
     <>
-    {
-        login ? 
+      {login ? (
         <>
-        <h1>welcome {username}</h1>
-        <button onClick={handleLogout}>LogOut</button>
-        </> :<form onSubmit={handleSubmit}>
-        <div className="heading">
-        <h1>Login</h1>
-        </div>
-        <div className="username">
-            <input type="text" name="username" id="username" onChange={handleUsername}/>
-        </div>
-        <div className="password">
-            <input type="password" name="password" id="password" onChange={handlePassword}/>
-        </div>
-        <button type='submit'>LogIn</button>
-    </form>
-    }
+          <h1>welcome {username}</h1>
+          <button onClick={handleLogout}>LogOut</button>
+        </>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="heading">
+            <h1>Login</h1>
+          </div>
+          <div className="username">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              onChange={handleUsername}
+            />
+          </div>
+          <div className="password">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={handlePassword}
+            />
+          </div>
+          <button type="submit">LogIn</button>
+        </form>
+      )}
     </>
-  )
+  );
 }
